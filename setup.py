4d823abf1a8193ccbc3d66f8d1becdad54904654
Ahmed Elnaggar <amit.najjar@gmail.com>
2019-11-04 10:48:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ ext_modules = []
 
 if "--pyprof" in sys.argv:
     with open('requirements.txt') as f:
-        required_packages = f.read().splitlines()
+        #required_packages = f.read().splitlines()
         pipmain(["install"] + required_packages)
     try:
         sys.argv.remove("--pyprof")
