878643d45a3841eecccccc1bbe602f4d012b13b4
Ahmed Elnaggar <amit.najjar@gmail.com>
2019-11-04 10:58:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -36,30 +36,30 @@ if TORCH_MAJOR == 0 and TORCH_MINOR < 4:
 cmdclass = {}
 ext_modules = []
 
-if "--pyprof" in sys.argv:
-    with open('requirements.txt') as f:
-        required_packages = f.read().splitlines()
-        #pipmain(["install"] + required_packages)
-    try:
-        sys.argv.remove("--pyprof")
-    except:
-        pass
-else:
-    warnings.warn("Option --pyprof not specified. Not installing PyProf dependencies!")
+if #"--pyprof" in sys.argv:
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+    #pipmain(["install"] + required_packages)
+try:
+    sys.argv.remove("--pyprof")
+except:
+    pass
+#else:
+#    warnings.warn("Option --pyprof not specified. Not installing PyProf dependencies!")
 
-if "--cpp_ext" in sys.argv or "--cuda_ext" in sys.argv:
-    if TORCH_MAJOR == 0:
-        raise RuntimeError("--cpp_ext requires Pytorch 1.0 or later, "
-                           "found torch.__version__ = {}".format(torch.__version__))
-    from torch.utils.cpp_extension import BuildExtension
-    cmdclass['build_ext'] = BuildExtension
+#if "--cpp_ext" in sys.argv or "--cuda_ext" in sys.argv:
+if TORCH_MAJOR == 0:
+    raise RuntimeError("--cpp_ext requires Pytorch 1.0 or later, "
+                       "found torch.__version__ = {}".format(torch.__version__))
+from torch.utils.cpp_extension import BuildExtension
+cmdclass['build_ext'] = BuildExtension
 
-if "--cpp_ext" in sys.argv:
-    from torch.utils.cpp_extension import CppExtension
-    sys.argv.remove("--cpp_ext")
-    ext_modules.append(
-        CppExtension('apex_C',
-                     ['csrc/flatten_unflatten.cpp',]))
+#if "--cpp_ext" in sys.argv:
+from torch.utils.cpp_extension import CppExtension
+sys.argv.remove("--cpp_ext")
+ext_modules.append(
+    CppExtension('apex_C',
+                 ['csrc/flatten_unflatten.cpp',]))
 
 def check_cuda_torch_binary_vs_bare_metal(cuda_dir):
     raw_output = subprocess.check_output([cuda_dir + "/bin/nvcc", "-V"], universal_newlines=True)
@@ -95,47 +95,47 @@ if (TORCH_MAJOR > 1) or (TORCH_MAJOR == 1 and TORCH_MINOR > 2):
     version_ge_1_3 = ['-DVERSION_GE_1_3']
 version_dependent_macros = version_ge_1_1 + version_ge_1_3
 
-if "--cuda_ext" in sys.argv:
-    from torch.utils.cpp_extension import CUDAExtension
-    sys.argv.remove("--cuda_ext")
+#if "--cuda_ext" in sys.argv:
+from torch.utils.cpp_extension import CUDAExtension
+sys.argv.remove("--cuda_ext")
 
-    if torch.utils.cpp_extension.CUDA_HOME is None:
-        raise RuntimeError("--cuda_ext was requested, but nvcc was not found.  Are you sure your environment has nvcc available?  If you're installing within a container from https://hub.docker.com/r/pytorch/pytorch, only images whose names contain 'devel' will provide nvcc.")
-    else:
-        check_cuda_torch_binary_vs_bare_metal(torch.utils.cpp_extension.CUDA_HOME)
+if torch.utils.cpp_extension.CUDA_HOME is None:
+    raise RuntimeError("--cuda_ext was requested, but nvcc was not found.  Are you sure your environment has nvcc available?  If you're installing within a container from https://hub.docker.com/r/pytorch/pytorch, only images whose names contain 'devel' will provide nvcc.")
+else:
+    check_cuda_torch_binary_vs_bare_metal(torch.utils.cpp_extension.CUDA_HOME)
 
-        ext_modules.append(
-            CUDAExtension(name='amp_C',
-                          sources=['csrc/amp_C_frontend.cpp',
-                                   'csrc/multi_tensor_sgd_kernel.cu',
-                                   'csrc/multi_tensor_scale_kernel.cu',
-                                   'csrc/multi_tensor_axpby_kernel.cu',
-                                   'csrc/multi_tensor_l2norm_kernel.cu',
-                                   'csrc/multi_tensor_lamb_stage_1.cu',
-                                   'csrc/multi_tensor_lamb_stage_2.cu',
-                                   'csrc/multi_tensor_adam.cu',
-                                   'csrc/multi_tensor_novograd.cu',
-                                   'csrc/multi_tensor_lamb.cu'],
-                          extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
-                                              'nvcc':['-lineinfo',
-                                                      '-O3',
-                                                      # '--resource-usage',
-                                                      '--use_fast_math'] + version_dependent_macros}))
-        ext_modules.append(
-            CUDAExtension(name='syncbn',
-                          sources=['csrc/syncbn.cpp',
-                                   'csrc/welford.cu'],
-                          extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
-                                              'nvcc':['-O3'] + version_dependent_macros}))
+    ext_modules.append(
+        CUDAExtension(name='amp_C',
+                      sources=['csrc/amp_C_frontend.cpp',
+                               'csrc/multi_tensor_sgd_kernel.cu',
+                               'csrc/multi_tensor_scale_kernel.cu',
+                               'csrc/multi_tensor_axpby_kernel.cu',
+                               'csrc/multi_tensor_l2norm_kernel.cu',
+                               'csrc/multi_tensor_lamb_stage_1.cu',
+                               'csrc/multi_tensor_lamb_stage_2.cu',
+                               'csrc/multi_tensor_adam.cu',
+                               'csrc/multi_tensor_novograd.cu',
+                               'csrc/multi_tensor_lamb.cu'],
+                      extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
+                                          'nvcc':['-lineinfo',
+                                                  '-O3',
+                                                  # '--resource-usage',
+                                                  '--use_fast_math'] + version_dependent_macros}))
+    ext_modules.append(
+        CUDAExtension(name='syncbn',
+                      sources=['csrc/syncbn.cpp',
+                               'csrc/welford.cu'],
+                      extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
+                                          'nvcc':['-O3'] + version_dependent_macros}))
 
-        ext_modules.append(
-            CUDAExtension(name='fused_layer_norm_cuda',
-                          sources=['csrc/layer_norm_cuda.cpp',
-                                   'csrc/layer_norm_cuda_kernel.cu'],
-                          extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
-                                              'nvcc':['-maxrregcount=50',
-                                                      '-O3',
-                                                      '--use_fast_math'] + version_dependent_macros}))
+    ext_modules.append(
+        CUDAExtension(name='fused_layer_norm_cuda',
+                      sources=['csrc/layer_norm_cuda.cpp',
+                               'csrc/layer_norm_cuda_kernel.cu'],
+                      extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
+                                          'nvcc':['-maxrregcount=50',
+                                                  '-O3',
+                                                  '--use_fast_math'] + version_dependent_macros}))
 
 if "--bnp" in sys.argv:
     from torch.utils.cpp_extension import CUDAExtension
